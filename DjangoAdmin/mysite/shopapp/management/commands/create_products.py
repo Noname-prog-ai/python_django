b53df0e2a99784af8848581e24da5f1b4b3785da
artem <andtorg55@>
2024-08-15 18:08:54 +0500

    finished work

--- DjangoAdmin/mysite/shopapp/management/commands/create_products.py
+++ DjangoAdmin/mysite/shopapp/management/commands/create_products.py
@@ -1,23 +1,16 @@
 from django.core.management.base import BaseCommand
 from shopapp.models import Product
 
+
 class Command(BaseCommand):
     help = 'Create sample products'
 
     def handle(self, *args, **kwargs):
-        # Определяем данные для продуктов
-        productsdata = [
-            {"name": "Product 1", "description": "Description 1", "price": 10.0, "quantity": 100},
-            {"name": "Product 2", "description": "Description 2", "price": 20.0, "quantity": 200},
-            {"name": "Product 3", "description": "Description 3", "price": 30.0, "quantity": 300},
-        ]
+        productsdata =
+        {"name": "Product 1", "description": "Description 1", "price": 10.0, "quantity": 100},
+        {"name": "Product 2", "description": "Description 2", "price": 20.0, "quantity": 200},
+        {"name": "Product 3", "description": "Description 3", "price": 30.0, "quantity": 300},
 
-        # Проходимся по данным продуктов
-        for data in productsdata:
-            # Создаем или находим продукт
-            product, created = Product.objects.get_or_create(**data)
-            # Выводим сообщение об успехе
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Product "{data["name"]}" created.'))
-            else:
-                self.stdout.write(self.style.SUCCESS(f'Product "{data["name"]}" already exists.'))
+    for data in productsdata:
+        Product.objects.getorcreate(**data)
+        self.stdout.write(self.style.SUCCESS(f'Product "{data["name"]}" created/exists.'))
--- DjangoAdmin/mysite/shopapp/models.py
+++ DjangoAdmin/mysite/shopapp/models.py
@@ -4,15 +4,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveSmallIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_available = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(max_length=255)  # Название продукта
+    description = models.TextField()  # Описание продукта
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Цена продукта
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # Скидка
+    archived = models.BooleanField(default=False)  # Поле для архивации
 
 class Order(models.Model):
     customer_name = models.CharField(max_length=255)
