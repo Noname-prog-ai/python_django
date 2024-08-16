cb2b1b7887a1531807d4abda5c02e30cf117bf2f
artem <andtorg55@>
2024-08-16 23:39:02 +0500

    finished work

--- DjangoAdmin/mysite/shopapp/models.py
+++ DjangoAdmin/mysite/shopapp/models.py
@@ -15,11 +15,9 @@ class Product(models.Model):
         return self.name
 
 class Order(models.Model):
-    customer_name = models.CharField(max_length=255)
-    customer_address = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)  # Связь с Product
+    quantity = models.IntegerField()  # Количество
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)  # Общая цена
 
     def __str__(self):
         return f"Order by {self.customer_name} at {self.created_at}"
