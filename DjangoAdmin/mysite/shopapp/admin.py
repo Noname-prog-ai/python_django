e0e5df2a4d9a789f079141d0ea075b9c18c160d4
artem <andtorg55@>
2024-08-19 18:26:45 +0500

    finished work

--- DjangoAdmin/mysite/shopapp/admin.py
+++ DjangoAdmin/mysite/shopapp/admin.py
@@ -3,35 +3,39 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product, Order
 
+class OrderInline(admin.TabularInline):
+    model = Order
+    extra = 1
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'discount', 'archived')  # Выводим эти поля в списке
-    search_fields = ('name', 'price')  # Поля для поиска
-
-    actions = ['archive_products']  # Добавляем действие для архивации
+    list_display = ('name', 'description', 'price', 'discount')
+    list_filter = ('price', 'discount')
+    search_fields = ('name', 'price',)
+    inlines = [OrderInline]
 
     fieldsets = (
         (None, {
             'fields': ('name', 'description')
         }),
-        ('Price Info', {
-            'fields': ('price', 'discount')
+        ('Цена', {
+            'fields': ('price', 'discount'),
         }),
-        ('Extra Options', {
-            'fields': ('archived',),
-            'classes': ('collapse',)  # Секция свернута
+        ('Дополнительные опции', {
+            'fields': ('is_archived',),
+            'classes': ('collapse',),
         }),
     )
 
-    def archive_products(self, request, queryset):
-        queryset.update(archived=True)
-        self.message_user(request, "Выбранные продукты были заархивированы.")
+    # Групповое действие для архивации
+    actions = ['archive_selected']
 
-    archive_products.short_description = "Заархивировать выделенные продукты"
-
+    def archive_selected(self, request, queryset):
+        queryset.update(is_archived=True)
+        self.message_user(request, "Выбранные записи успешно заархивированы.")
+    archive_selected.short_description = "Архивировать выбранные записи"
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('product', 'quantity', 'total_price')  # Поля для отображения
-    search_fields = ('product__name', 'quantity')  # Поля для поиска
+    list_display = ('product', 'quantity', 'order_date')
+    search_fields = ('product__name',)  # Поиск по имени продукта
