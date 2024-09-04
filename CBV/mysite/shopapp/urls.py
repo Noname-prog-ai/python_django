9c358f5700845d49584347e4edd369db40154d17
artem <andtorg55@>
2024-09-04 18:09:26 +0500

    completed work

--- CBV/mysite/shopapp/urls.py
+++ CBV/mysite/shopapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import shop_index, groups_list, products_list, orders_list
+from .views import ShopIndexView, GroupsListView, ProductsListView, OrdersListView
 
 app_name = "shopapp"
 
 urlpatterns = [
-    path("", shop_index, name="index"),
-    path("groups/", groups_list, name="groups_list"),
-    path("products/", products_list, name="products_list"),
-    path("orders/", orders_list, name="orders_list"),
+    path('', ShopIndexView.as_view(), name='shop_index'),
+    path('groups/', GroupsListView.as_view(), name='groups_list'),
+    path('products/', ProductsListView.as_view(), name='products_list'),
+    path('orders/', OrdersListView.as_view(), name='orders_list'),
 ]
--- CBV/mysite/shopapp/views.py
+++ CBV/mysite/shopapp/views.py
@@ -1,41 +1,45 @@
 from timeit import default_timer
-
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
-
+from django.views import View
+from django.views.generic import ListView
 from .models import Product, Order
 
 
-def shop_index(request: HttpRequest):
-    products = [
-        ('Laptop', 1999),
-        ('Desktop', 2999),
-        ('Smartphone', 999),
-    ]
-    context = {
-        "time_running": default_timer(),
-        "products": products,
-    }
-    return render(request, 'shopapp/shop-index.html', context=context)
+class ShopIndexView(View):
+    def get(self, request: HttpRequest):
+        products = [
+            ('Laptop', 1999),
+            ('Desktop', 2999),
+            ('Smartphone', 999),
+        ]
+        context = {
+            "time_running": default_timer(),
+            "products": products,
+        }
+        return render(request, 'shopapp/shop-index.html', context=context)
 
 
-def groups_list(request: HttpRequest):
-    context = {
-        "groups": Group.objects.prefetch_related('permissions').all(),
-    }
-    return render(request, 'shopapp/groups-list.html', context=context)
+class GroupsListView(ListView):
+    model = Group
+    template_name = 'shopapp/groups-list.html'
+    context_object_name = 'groups'
+
+    def get_queryset(self):
+        return self.model.objects.prefetch_related('permissions').all()
 
 
-def products_list(request: HttpRequest):
-    context = {
-        "products": Product.objects.all(),
-    }
-    return render(request, 'shopapp/products-list.html', context=context)
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'shopapp/products-list.html'
+    context_object_name = 'products'
 
 
-def orders_list(request: HttpRequest):
-    context = {
-        "orders": Order.objects.select_related("user").prefetch_related("products").all(),
-    }
-    return render(request, 'shopapp/orders-list.html', context=context)
+class OrdersListView(ListView):
+    model = Order
+    template_name = 'shopapp/orders-list.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        return self.model.objects.select_related("user").prefetch_related("products").all()
