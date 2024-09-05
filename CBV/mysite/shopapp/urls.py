7a11ba285b8446c0103ac41c23796465e1171001
artem <andtorg55@>
2024-09-05 19:00:43 +0500

    completed work

--- CBV/mysite/shopapp/urls.py
+++ CBV/mysite/shopapp/urls.py
@@ -1,12 +1,30 @@
 from django.urls import path
-
-from .views import ShopIndexView, GroupsListView, ProductsListView, OrdersListView
-
-app_name = "shopapp"
+from .views import (
+    ShopIndexView,
+    GroupsListView,
+    ProductsListView,
+    OrdersListView,
+    ProductDetailsView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+    OrderDetailsView,
+    OrderCreateView,
+    OrderUpdateView,
+    OrderDeleteView,
+)
 
 urlpatterns = [
     path('', ShopIndexView.as_view(), name='shop_index'),
     path('groups/', GroupsListView.as_view(), name='groups_list'),
     path('products/', ProductsListView.as_view(), name='products_list'),
+    path('products/<int:pk>/', ProductDetailsView.as_view(), name='product_detail'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('orders/', OrdersListView.as_view(), name='orders_list'),
+    path('orders/<int:pk>/', OrderDetailsView.as_view(), name='order_detail'),
+    path('orders/create/', OrderCreateView.as_view(), name='order_create'),
+    path('orders/<int:pk>/edit/', OrderUpdateView.as_view(), name='order_update'),
+    path('orders/<int:pk>/delete/', OrderDeleteView.as_view(), name='order_delete'),
 ]
--- CBV/mysite/shopapp/views.py
+++ CBV/mysite/shopapp/views.py
@@ -1,10 +1,11 @@
 from timeit import default_timer
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.views.generic import ListView
 from .models import Product, Order
+from .forms import ProductForm, OrderForm
 
 
 class ShopIndexView(View):
@@ -43,3 +44,95 @@ class OrdersListView(ListView):
 
     def get_queryset(self):
         return self.model.objects.select_related("user").prefetch_related("products").all()
+
+
+# Продукт Details View
+class ProductDetailsView(View):
+    def get(self, request: HttpRequest, pk: int):
+        product = get_object_or_404(Product, pk=pk)
+        return render(request, 'shopapp/product_detail.html', {'product': product})
+
+# Продукт Create View
+class ProductCreateView(View):
+    def get(self, request: HttpRequest):
+        form = ProductForm()
+        return render(request, 'shopapp/product_form.html', {'form': form})
+
+    def post(self, request: HttpRequest):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('products_list')
+        return render(request, 'shopapp/product_form.html', {'form': form})
+
+# Продукт Update View
+class ProductUpdateView(View):
+    def get(self, request: HttpRequest, pk: int):
+        product = get_object_or_404(Product, pk=pk)
+        form = ProductForm(instance=product)
+        return render(request, 'shopapp/product_form.html', {'form': form})
+
+    def post(self, request: HttpRequest, pk: int):
+        product = get_object_or_404(Product, pk=pk)
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('products_list')
+        return render(request, 'shopapp/product_form.html', {'form': form})
+
+# Продукт Delete View
+class ProductDeleteView(View):
+    def get(self, request: HttpRequest, pk: int):
+        product = get_object_or_404(Product, pk=pk)
+        return render(request, 'shopapp/product_confirm_delete.html', {'product': product})
+
+    def post(self, request: HttpRequest, pk: int):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return redirect('products_list')
+
+
+# Заказ Details View
+class OrderDetailsView(View):
+    def get(self, request: HttpRequest, pk: int):
+        order = get_object_or_404(Order, pk=pk)
+        return render(request, 'shopapp/order_detail.html', {'order': order})
+
+# Заказ Create View
+class OrderCreateView(View):
+    def get(self, request: HttpRequest):
+        form = OrderForm()
+        return render(request, 'shopapp/order_form.html', {'form': form})
+
+    def post(self, request: HttpRequest):
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('orders_list')
+        return render(request, 'shopapp/order_form.html', {'form': form})
+
+# Заказ Update View
+class OrderUpdateView(View):
+    def get(self, request: HttpRequest, pk: int):
+        order = get_object_or_404(Order, pk=pk)
+        form = OrderForm(instance=order)
+        return render(request, 'shopapp/order_form.html', {'form': form})
+
+    def post(self, request: HttpRequest, pk: int):
+        order = get_object_or_404(Order, pk=pk)
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('orders_list')
+        return render(request, 'shopapp/order_form.html', {'form': form})
+
+# Заказ Delete View
+class OrderDeleteView(View):
+    def get(self, request: HttpRequest, pk: int):
+        order = get_object_or_404(Order, pk=pk)
+        return render(request, 'shopapp/order_confirm_delete.html', {'order': order})
+
+    def post(self, request: HttpRequest, pk: int):
+        order = get_object_or_404(Order, pk=pk)
+        order.delete()
+        return redirect('orders_list')
