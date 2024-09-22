fee9dbaa03bef9375d63dd74449b220f46d0e0c1
artem <andtorg55@>
2024-09-22 21:32:39 +0500

    completed work

--- Testing/mysite/shopapp/tests.py
+++ Testing/mysite/shopapp/tests.py
@@ -50,55 +50,37 @@ class OrderDetailViewTest(TestCase):
 
 class OrdersExportViewTest(TestCase):
 
+    fixtures = ['fixtures.yaml']
+
     @classmethod
     def setUpTestData(cls):
-        # Создание пользователя с правами
         cls.user = User.objects.create_user(username='staffuser', password='staffpassword', is_staff=True)
         permission = Permission.objects.get(codename='view_order')
         cls.user.user_permissions.add(permission)
 
-        # Создание продуктов
-        cls.product1 = Product.objects.create(name='Product 1')
-        cls.product2 = Product.objects.create(name='Product 2')
-
-        # Создание заказов и связывание с продуктами
-        cls.order1 = Order.objects.create(user=cls.user, delivery_address='Address 1', promocode='CODE1')
-        cls.order1.products.add(cls.product1)
-        cls.order2 = Order.objects.create(user=cls.user, delivery_address='Address 2', promocode='CODE2')
-        cls.order2.products.add(cls.product2)
-
     def setUp(self):
-        # Вход пользователя с правами
         self.client.login(username='staffuser', password='staffpassword')
-
-    def tearDown(self):
-        # Удаление заказов
-        self.order1.delete()
-        self.order2.delete()
-        # Удаление пользователя и продуктов
-        self.product1.delete()
-        self.product2.delete()
-        self.user.delete()
 
     def test_orders_export_view(self):
         response = self.client.get(reverse('shopapp:orders_export'))
 
-        # Проверка статуса ответа
         self.assertEqual(response.status_code, 200)
 
-        # Получение всех заказов
-        orders = Order.objects.select_related('user').prefetch_related('products').all()
-
-        # Проверка структуры ответа
         expected_data = [
             {
-                'id': order.pk,
-                'delivery_address': order.delivery_address,
-                'promocode': order.promocode,
-                'user_id': order.user.pk,
-                'product_ids': [product.pk for product in order.products.all()]
+                'pk': 1,
+                'promocode': 'CODE1',
+                'delivery_address': 'Address 1',
+                'user_pk': 1,
+                'products': [1]
+            },
+            {
+                'pk': 2,
+                'promocode': 'CODE2',
+                'delivery_address': 'Address 2',
+                'user_pk': 1,
+                'products': []
             }
-            for order in orders
         ]
 
-        self.assertJSONEqual(response.content, expected_data)
+        self.assertJSONEqual(response.content.decode(), {'orders': expected_data})
