0fcae54bf7be2d64e015259ae4e39d74420f141d
artem <andtorg55@>
2024-10-14 18:01:14 +0500

    completed work

--- logging/mysite/mysite/urls.py
+++ logging/mysite/mysite/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shopapp.urls')),
     path('myauth/', include('myauth.urls')),
+    path('debug/', include('debug_toolbar.urls')),
 ]
 
 if settings.DEBUG:
